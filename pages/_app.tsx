ecb7477aef99cbd79f3c69b95b250efca178c5df
mbhardwaj-caravellabs <mbhardwaj@caravellabs.com>
2021-08-20 14:09:08 +0530

    First Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import  theme from '../theme/theme'
+import { CssBaseline } from '@material-ui/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ThemeProvider theme={theme}> <CssBaseline />< Component {...pageProps} /></ThemeProvider>
+  )
 }
 export default MyApp
