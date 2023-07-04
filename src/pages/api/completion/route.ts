4e84ef836c0a41aa969d0fd8f7caf59a44761780
Subham Kundu <skundu@caravellabs.com>
2023-07-05 04:41:44 +0530

    removed console.log

--- src/pages/api/completion/route.ts
+++ src/pages/api/completion/route.ts
@@ -1,13 +1,11 @@
 import { Configuration, OpenAIApi } from 'openai-edge'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 
-// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(config)
 
-// Set the runtime to edge for best performance
 export const runtime = 'edge'
 
 export default async function POST(req: Request) {
@@ -32,7 +30,6 @@ export default async function POST(req: Request) {
     })
     // Convert the response into a friendly text-stream
     const stream = OpenAIStream(response)
-    console.log(response)
     // Respond with the stream
     return new StreamingTextResponse(stream)
 }
